9a7a00a327a818362aa4c9388ea141a2526b4e09
lowkey <saltyndea@gmail.com>
2021-09-28 14:23:56 -0600

    Initial commit

---
+++ src/planning.js
@@ -0,0 +1,25 @@
+/*
+ON LOAD
+
+Set state to comments list stored in firebase if any through a use effect/ use state
+
+Write an error handling function for if database is down
+
+Use a map to render any existing comments
+
+The send message component will need an onClick function on its submit button to push the comment to state which should trigger a render and show the new comment after being added to state as well as save to firebase?
+
+The clear chat button in the header will delete state
+
+Components:
+- Header 
+- Comment
+- Send Message
+
+STRETCH GOALS
+- Add authentication Sign up/Sign in - user made not google
+- Add welcome message with username in header
+- Add username property to firebase comments
+- Add username to their comments
+
+*/
