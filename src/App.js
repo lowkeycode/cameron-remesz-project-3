453cc5d15437022e54879a5ead40ebfe8783aa1a
lowkey <saltyndea@gmail.com>
2021-09-29 07:49:00 -0600

    Started header component layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Header from './components/Header';
+
 import './styles.scss';
-import logo from './chatter-logo.svg'
 
 function App() {
   return (
     <main className="main">
-      <h1>Chatter</h1>
+      <Header/>
     </main>
   );
 }
