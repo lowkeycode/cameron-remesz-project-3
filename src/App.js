033e4b3819cdc404944e330ea032b7163776c1fb
lowkey <saltyndea@gmail.com>
2021-10-02 21:49:43 -0600

    Added minor date styling

--- src/App.js
+++ src/App.js
@@ -1,39 +1,51 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-import { ref, onValue, push, remove } from 'firebase/database';
+import { ref, onValue, push, remove } from "firebase/database";
 
-import realtime from './firebase';
+import realtime from "./firebase";
 
-import Header from './components/Header';
-import Comments from './components/Comments';
-import Send from './components/Send';
+import Header from "./components/Header";
+import Comments from "./components/Comments";
+import Send from "./components/Send";
 
-import './styles.scss';
+import "./styles.scss";
 
 function App() {
-
-  const [ commentList, setCommentList ] = useState([]);
-
+  const [commentList, setCommentList] = useState([]);
 
   useEffect(() => {
-
     const dbRef = ref(realtime);
 
-    onValue(dbRef, snapshot => {
+    onValue(dbRef, (snapshot) => {
       const comments = snapshot.val();
+
+      console.log("Snapshot.val():", snapshot.val());
 
       const dbList = [];
 
-      
-    });
+      for (let comment in comments) {
+        //todo Make key the hash from firebase and pass through props to each comment
+        const commentObj = {
+          key: comment,
+          userName: comments[comment].userName,
+          comment: comments[comment].comment,
+          commentDate: comments[comment].commentDate,
+        };
 
+        dbList.push(commentObj);
+      }
+
+      setCommentList(dbList);
+    });
   }, []);
+
+  console.log(commentList);
 
   return (
     <main className="main">
-      <Header/>
-      <Comments/>
-      <Send/>
+      <Header />
+      <Comments commentList={commentList} />
+      <Send />
     </main>
   );
 }
--- src/components/Send.js
+++ src/components/Send.js
@@ -1,8 +1,8 @@
 const Send = () => {
   return (
     <form className="send">
-      <div class="send__container">
-        <label className ="sr-only" for="comment">Write you comment here</label>
+      <div className="send__container">
+        <label className ="sr-only" htmlFor="comment">Write you comment here</label>
         <input type="text" id="comment"/>
         <button>Send</button>
       </div>
